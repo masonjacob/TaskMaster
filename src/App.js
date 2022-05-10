d0c83bd260d4178f18369b2d6643ebc43ecc37ca
Mason Jacob <masonjacob@uri.edu>
2022-05-09 23:19:39 -0400

    Addition of Basic Header Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import Header from './components/Header'
+import Calendar from './components/Calendar';
 import './App.css';
 
 function App() {
   return (
-  <FullCalendar
-  plugins={[ dayGridPlugin ]}
-  initialView="dayGridMonth"
-  weekends={true}
-  events={[
-    { title: 'event 1', date: '2022-05-01', color: 'green', },
-    { title: 'event 2', date: '2022-05-02', color: 'red' }
-  ]}
-  />
+    <div>
+      <div style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#00ffdd"
+  }}>
+      <Header/>
+      </div>
+  <Calendar/>
+    </div>
+
   )
 }
 
 export default App;
+
