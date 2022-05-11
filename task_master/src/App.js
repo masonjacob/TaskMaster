f86bd168860a7dcbe48a2183a875f1a7bde05024
Mason Jacob <masonjacob@uri.edu>
2022-05-11 17:10:41 -0400

    Addition of Cosas

--- task_master/src/App.js
+++ task_master/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Calendar from './components/Calendar';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
